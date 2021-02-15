9703273b5a057b9fd648a4f1f2dbe9cc9a949e00
SessevmezBugra <sessevmezbugra@gmail.com>
2021-02-15 14:50:11 +0300

    3 files modified, 9 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
-import { booksReducer } from './state/books.reducer';
-import { collectionReducer } from './state/collection.reducer';
-import { StoreModule } from '@ngrx/store';
+import { booksReducer } from "./state/books.reducer";
+import { collectionReducer } from "./state/collection.reducer";
+import { StoreModule } from "@ngrx/store";
+import { BookCollectionComponent } from "./book-collection/book-collection.component";
+import { BookListComponent } from "./book-list/book-list.component";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-    imports: [
-      BrowserModule,
-      StoreModule.forRoot({ books: booksReducer, collection: collectionReducer }),
-      HttpClientModule,
-    ],
-    declarations: [AppComponent],
-    bootstrap: [AppComponent],
-  })
-  export class AppModule {}
-
-
-/*
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://github.com/ngrx/platform
-*/
+  imports: [
+    BrowserModule,
+    StoreModule.forRoot({ books: booksReducer, collection: collectionReducer }),
+    HttpClientModule
+  ],
+  declarations: [AppComponent, BookListComponent, BookCollectionComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
